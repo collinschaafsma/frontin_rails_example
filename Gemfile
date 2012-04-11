8aca67d1a908fe94117e759dbd67fddd6886677d
Collin Schaafsma <collin@quickleft.com>
2012-04-10 20:57:19 -0600

    adding frontin and generating the backbone boilerplate

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
 
+gem 'frontin', :group => :development
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 FrontinRailsExample::Application.configure do
+  config.middleware.use(Frontin::Middleware, :paths => ['/assets', '/app'], :root => 'frontend')
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
